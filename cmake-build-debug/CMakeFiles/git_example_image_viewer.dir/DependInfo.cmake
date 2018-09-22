afd2bc3674c2c885e925fe700993b707e7e5d5b3
rengega <renigega@outlook.it>
2018-09-22 13:59:13 +0200

    commit master before adding branch 1

---
+++ cmake-build-debug/CMakeFiles/git_example_image_viewer.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ren/Desktop/Programmazione/laboratorio/num3_18Aprile/git_example_image_viewer/BitmapImage.cpp" "/home/ren/Desktop/Programmazione/laboratorio/num3_18Aprile/git_example_image_viewer/cmake-build-debug/CMakeFiles/git_example_image_viewer.dir/BitmapImage.cpp.o"
+  "/home/ren/Desktop/Programmazione/laboratorio/num3_18Aprile/git_example_image_viewer/main.cpp" "/home/ren/Desktop/Programmazione/laboratorio/num3_18Aprile/git_example_image_viewer/cmake-build-debug/CMakeFiles/git_example_image_viewer.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/git_example_image_viewer.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/git_example_image_viewer.dir/main.cpp.o"
+  "CMakeFiles/git_example_image_viewer.dir/BitmapImage.cpp.o"
+  "git_example_image_viewer.pdb"
+  "git_example_image_viewer"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/git_example_image_viewer.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
